fbb2cd17c53c673f509314db604bb1d6f692e570
liangyin6070 <liudw2@163.com>
2016-12-30 00:07:48 +0800

    测试上传代码

--- src/main/java/com/liupro/web/manage/controller/CommonController.java
+++ src/main/java/com/liupro/web/manage/controller/CommonController.java
@@ -1,12 +1,27 @@
 package com.liupro.web.manage.controller;
 
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.jeecgframework.poi.excel.ExcelImportUtil;
+import org.jeecgframework.poi.excel.entity.ImportParams;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
+import com.alibaba.fastjson.JSONObject;
 import com.framwork.core.spring.BaseRestController;
+import com.framwork.utils.ResponseUtils;
+import com.liupro.web.manage.model.Demo;
 /**
  * 
  * @description 
@@ -21,5 +36,44 @@ public class CommonController extends BaseRestController {
 	@RequestMapping(value="/common/blank",method=RequestMethod.GET)
 	public String toBlank() {
 		return "/admin/blank";
+	}
+	
+	@RequestMapping(value="/common/upload/demo",method=RequestMethod.POST)
+	public void upload(@RequestParam(value = "demo_file", required = false) MultipartFile file, 
+			HttpServletRequest request, HttpServletResponse response) {
+//    	String path = request.getSession().getServletContext().getRealPath("upload");  
+//	      String fileName = file.getOriginalFilename();  
+//	      File targetFile = new File(path, fileName);  
+//	      if(!targetFile.exists()){  
+//	          targetFile.mkdirs();  
+//	      }  
+//	      //保存  
+//	      try {  
+//	          file.transferTo(targetFile); 
+//	      } catch (Exception e) {  
+//	          e.printStackTrace();  
+//	      }  
+    //  model.addAttribute("fileUrl", request.getContextPath()+"/upload/"+fileName); 
+		
+		JSONObject obj = new JSONObject();
+		ImportParams params = new ImportParams();
+		params.setTitleRows(1);
+		params.setHeadRows(1);
+//		params.setNeedSave(true);
+//		params.setSaveUrl("upload");
+		try {
+			List<Demo> list = ExcelImportUtil.importExcel(file.getInputStream(), Demo.class, params);
+			for (Demo demo : list) {
+				System.out.println(demo.getId() + "," + demo.getName() + "," + demo.getAddress());
+			}
+			obj.put("success", true);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		ResponseUtils.renderJson(response, obj.toJSONString());
 	}
 }
--- src/main/webapp/webpage/admin/blank.jsp
+++ src/main/webapp/webpage/admin/blank.jsp
@@ -7,13 +7,52 @@
 <title>首页</title>
 <jsp:include page="/common/meta.jsp" />
 <jsp:include page="/common/easyui.jsp" />
+
+<link rel="stylesheet" type="text/css" href="${commonPath}/uploadify/uploadify.css" />
+<script type="text/javascript" src="${commonPath}/uploadify/jquery.uploadify.min.js"></script>
 </head>
 <body>
 <h2>欢迎使用粤西后台管理系统</h2>
-<div id="panel_new_news" class="easyui-panel" title="My Panel" style="width:500px;height:150px;padding:10px;"   
+<!-- <div id="panel_new_news" class="easyui-panel" title="My Panel" style="width:500px;height:150px;padding:10px;"   
 	data-options="iconCls:'icon-save',closable:true, collapsible:true,minimizable:true,maximizable:true">   
     <p>panel content.</p>   
     <p>panel content.</p>   
-</div> 
+</div>  -->
+<div>
+	<table>
+		<tr>
+			<td><label>上传课件：</td>
+            <td><label><input type="file" name="demo_file" id="uploadify"/></label>&nbsp;&nbsp;</td>
+            <td><div id="fileQueue"></div></td>
+		</tr>
+	</table>
+</div>
 </body>
+<script type="text/javascript">
+$(function(){
+	$("#uploadify").uploadify({
+        'uploader': '${basePath}/common/upload/demo',
+        'swf':"${commonPath}/uploadify/uploadify.swf",
+        'cancelImg': '${commonPath}/uploadify/cancel.png',
+        'queueID': 'fileQueue',
+        'auto': true,
+        'multi':false,
+        'buttonText': '选择文件',
+        'fileSizeLimit':'100MB',
+        'simUploadLimit' : 1,
+        'queueSizeLimit' : 1,
+        'fileExt': '*.xls',
+        'method':'post'
+        /* onComplete: function(event, queueID, fileObj, response, data) {
+            
+        },
+        onSelect:function(){
+        	
+        },
+        onError: function(event, queueID, fileObj) {
+            alert("文件:" + fileObj.name + "上传失败");
+        } */
+    });
+});
+</script>
 </html>
